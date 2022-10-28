034d552911f565f3088ad8e28d02ee9d6520e96f
Louis2602 <trantunglam135790@gmail.com>
2022-10-28 21:42:46 +0700

    update

--- 21127337.cpp
+++ 21127337.cpp
@@ -23,85 +23,112 @@ void Dec2Bin(char n, char arr[8])
         k & 1 ? arr[7 - i] = 1 : arr[7 - i] = 0;
     }
 }
-char Add(char a, char b)
+char *Add(char a[8], char b[8])
 {
-    if (b == 0)
-        return a;
-    return Add(a ^ b, (unsigned)(a & b) << 1);
-}
-char Subtract(char a, char b)
-{
-    b = ~b + 1;
-    return Add(a, b);
-}
-char Multiply(char Q, char M)
-{
-    // using Booth's Multiplication algorithm
-    char A = 0, QNegative1 = 0;
-    int k = 8;
-    char twoLastBit;
-    while (k > 0)
+    char *ans = new char(8);
+    char carry = 0;
+    for (int i = 7; i >= 0; i--)
     {
-        // last bit of Q
-        char Q0 = Q & 1;
-        twoLastBit = Q0 << 1 | QNegative1;
-        if (twoLastBit == 2)
-        {
-            A = Subtract(A, M);
-        }
-        else if (twoLastBit == 1)
-        {
-            A = Add(A, M);
-        }
-        QNegative1 = Q & 1, Q >>= 1;
-        Q ^= (-(A & 1) ^ Q) & (1UL << 7);
-        A >>= 1;
-        k--;
+        char sum = (a[i] ^ b[i] ^ carry);
+        ans[i] = sum;
+        carry = (a[i] & b[i]) | (b[i] & carry) | (a[i] & carry);
     }
-    char ans = A << 8 | Q;
+    if (carry)
+        ans[7] = carry;
     return ans;
 }
-char Divide(char Q, char M, int mode)
+char *Subtract(char a[8], char b[8])
 {
-    char A = 0;
-    int k = 8;
-    bool checkNegative = false;
-    if (M == 0)
-        return '\0';
-    if ((M && Q) || (!M && !Q))
-        checkNegative = false;
-    if (Q < 0 && M > 0)
-    {
-        Q *= -1;
-        checkNegative = true;
-    }
-    if (M < 0 && Q > 0)
-    {
-        M *= -1;
-        checkNegative = true;
-    }
-    while (k > 0)
-    {
-        A <<= 1;
-        A ^= (-((Q >> 7) & 1) ^ A) & (1UL);
-        Q <<= 1;
-        A = Subtract(A, M);
-        if (A < 0)
-        {
-            Q ^= (-0 ^ Q) & (1UL);
-            A = Add(A, M);
-        }
-        else
-            Q ^= (-1 ^ Q) & (1UL);
-        k--;
-    }
-    char ans;
-    if (mode)
-        checkNegative ? ans = Q *-1 : ans = Q;
-    else
-        checkNegative ? ans = A *-1 : ans = A;
+    for (int i = 0; i < 8; i++)
+        b[i] = !b[i];
+    // 11111010
+    char *tmp = new char(8);
+    char *ans = new char(8);
+    char one[8] = {1};
+    b = Add(b, one);
+    output(b);
+    ans = Add(a, b);
     return ans;
 }
+// char Add(char a, char b)
+// {
+//     if (b == 0)
+//         return a;
+//     return Add(a ^ b, (unsigned)(a & b) << 1);
+// }
+// char Subtract(char a, char b)
+// {
+//     b = ~b + 1;
+//     return Add(a, b);
+// }
+// char Multiply(char Q, char M)
+// {
+//     // using Booth's Multiplication algorithm
+//     char A = 0, QNegative1 = 0;
+//     int k = 8;
+//     char twoLastBit;
+//     while (k > 0)
+//     {
+//         // last bit of Q
+//         char Q0 = Q & 1;
+//         twoLastBit = Q0 << 1 | QNegative1;
+//         if (twoLastBit == 2)
+//         {
+//             A = Subtract(A, M);
+//         }
+//         else if (twoLastBit == 1)
+//         {
+//             A = Add(A, M);
+//         }
+//         QNegative1 = Q & 1, Q >>= 1;
+//         Q ^= (-(A & 1) ^ Q) & (1UL << 7);
+//         A >>= 1;
+//         k--;
+//     }
+//     char ans = A << 8 | Q;
+//     return ans;
+// }
+// char Divide(char Q, char M, int mode)
+// {
+//     char A = 0;
+//     int k = 8;
+//     bool checkNegative = false;
+//     if (M == 0)
+//         return '\0';
+//     if ((M && Q) || (!M && !Q))
+//         checkNegative = false;
+//     if (Q < 0 && M > 0)
+//     {
+//         Q *= -1;
+//         checkNegative = true;
+//     }
+//     if (M < 0 && Q > 0)
+//     {
+//         M *= -1;
+//         checkNegative = true;
+//     }
+//     while (k > 0)
+//     {
+//         A <<= 1;
+//         A ^= (-((Q >> 7) & 1) ^ A) & (1UL);
+//         Q <<= 1;
+//         A = Subtract(A, M);
+//         if (A < 0)
+//         {
+//             Q ^= (-0 ^ Q) & (1UL);
+//             A = Add(A, M);
+//         }
+//         else
+//             Q ^= (-1 ^ Q) & (1UL);
+//         k--;
+//     }
+//     char ans;
+//     if (mode)
+//         checkNegative ? ans = Q *-1 : ans = Q;
+//     else
+//         checkNegative ? ans = A *-1 : ans = A;
+//     return ans;
+// }
 int main()
 {
     int aDec, bDec;
@@ -118,35 +145,35 @@ int main()
     cout << "B (Binary): ";
     output(arrB);
 
-    char ans, arrAns[8];
-
-    ans = Add(a, b);
-    Dec2Bin(ans, arrAns);
+    char *arrAns = new char(8);
+    // ans = Add(a, b);
+    // Dec2Bin(ans, arrAns);
     cout << "A + B (Binary): ";
+    // output(arrAns);
+    // cout << "A + B (Decimal): " << (int)ans << '\n';
+    arrAns = Add(arrA, arrB);
     output(arrAns);
-    cout << "A + B (Decimal): " << (int)ans << '\n';
-
-    ans = Subtract(a, b);
-    Dec2Bin(ans, arrAns);
+    // Dec2Bin(ans, arrAns);
     cout << "A - B (Binary): ";
+    arrAns = Subtract(arrA, arrB);
     output(arrAns);
-    cout << "A - B (Decimal): " << (int)ans << '\n';
+    // cout << "A - B (Decimal): " << (int)ans << '\n';
 
-    ans = Multiply(a, b);
-    Dec2Bin(ans, arrAns);
-    cout << "A * B (Binary): ";
-    output(arrAns);
-    cout << "A * B (Decimal): " << (int)ans << '\n';
+    // ans = Multiply(a, b);
+    // Dec2Bin(ans, arrAns);
+    // cout << "A * B (Binary): ";
+    // output(arrAns);
+    // cout << "A * B (Decimal): " << (int)ans << '\n';
 
-    ans = Divide(a, b, 1);
-    Dec2Bin(ans, arrAns);
-    cout << "A / B (Binary): ";
-    output(arrAns);
-    cout << "A / B (Decimal): " << (int)ans << '\n';
-    ans = Divide(a, b, 0);
-    Dec2Bin(ans, arrAns);
-    cout << "A % B (Binary): ";
-    output(arrAns);
-    cout << "A % B (Decimal): " << (int)ans << '\n';
+    // ans = Divide(a, b, 1);
+    // Dec2Bin(ans, arrAns);
+    // cout << "A / B (Binary): ";
+    // output(arrAns);
+    // cout << "A / B (Decimal): " << (int)ans << '\n';
+    // ans = Divide(a, b, 0);
+    // Dec2Bin(ans, arrAns);
+    // cout << "A % B (Binary): ";
+    // output(arrAns);
+    // cout << "A % B (Decimal): " << (int)ans << '\n';
     return 0;
 }
